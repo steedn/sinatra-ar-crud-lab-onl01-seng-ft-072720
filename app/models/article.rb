c9c866d1f8a9d01f64bccb902d68b7e1a54ce03b
Nathan Steed <steedn@outlook.com>
2020-09-18 01:57:07 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  attr_accessor :title, :content
+  @@all = []
+  def initialize(name, content)
+    @name, @content = name, content
+    @@all << self
+  end
 end
